8969b00158668ddc7e0cf5de0584b0e4dfc4db86
vremayvnikuda <nevsky.andrew@gmail.com>
2022-12-20 01:34:32 +0700

    add wallet and privateKey +publicKey

---
+++ src/Wallet.java
@@ -0,0 +1,20 @@
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+
+public class Wallet {
+    public PrivateKey privateKey;
+    public PublicKey publicKey;
+    public Wallet(){
+        generateKeyPair();
+    }
+    public void generateKeyPair(){
+        try{
+            KeyPairGenerator keyGen=KeyPairGenerator.getInstance("ECDSA","BC");
+            SecureRandom random=SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec=new ECGenParameterSpec("prime192v1");
+            keyGen.initialize(ecSpec,random);
+            KeyPair keyPair=keyGen.generateKeyPair();
+
+        }
+    }
+}
