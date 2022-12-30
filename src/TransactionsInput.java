ebb2a8ee682b245a5243cf7b232c2623d432aa84
vremayvnikuda <nevsky.andrew@gmail.com>
2022-12-30 18:47:30 +0700

    add input and output transaction

---
+++ src/TransactionsInput.java
@@ -0,0 +1,8 @@
+public class TransactionsInput {
+    public String transactionOutputId;
+    public TransactionsInput UTXO;
+
+    public TransactionsInput(String transactionOutputId){
+        this.transactionOutputId=transactionOutputId;
+    }
+}
